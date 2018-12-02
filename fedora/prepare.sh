dbea493d34102d8764f55544bd64cc73edd3d1c2
Jordan Petridis <jpetridis@gnome.org>
2018-12-02 15:47:12 +0200

    Add gettext dependency
    It was already there, but apparently it also needed autopoint,
    or whaterver to function.

--- fedora/prepare.sh
+++ fedora/prepare.sh
@@ -5,4 +5,5 @@ dnf install -y gcc rust rust-std-static cargo make vala \
     automake autoconf libtool gettext itstool \
     gdk-pixbuf2-devel gobject-introspection-devel \
     gtk-doc git redhat-rpm-config gtk3-devel ccache \
-    libxml2-devel libcroco-devel cairo-devel pango-devel
+    libxml2-devel libcroco-devel cairo-devel pango-devel \
+    gettext-devel
--- opensuse/prepare.sh
+++ opensuse/prepare.sh
@@ -5,4 +5,4 @@ zypper install -y gcc rust rust-std cargo make vala \
     automake autoconf libtool gettext itstool git \
     gtk-doc gobject-introspection-devel gtk3-devel \
     libxml2-devel libcroco-devel cairo-devel \
-    pango-devel gdk-pixbuf-devel ccache
+    pango-devel gdk-pixbuf-devel ccache gettext-tools
