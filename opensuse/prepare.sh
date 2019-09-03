6d06676e3b3284839fd67fe57adf98ac3dc6cdda
Federico Mena Quintero <federico@gnome.org>
2019-09-03 08:58:45 -0500

    Add cargo-vendor to the openSUSE packages

--- opensuse/prepare.sh
+++ opensuse/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 zypper update -y
-zypper install -y gcc rust rust-std cargo make vala \
+zypper install -y gcc rust rust-std cargo cargo-vendor make vala \
     automake autoconf libtool gettext itstool git \
     gtk-doc gobject-introspection-devel gtk3-devel \
     libxml2-devel libcroco-devel cairo-devel \
