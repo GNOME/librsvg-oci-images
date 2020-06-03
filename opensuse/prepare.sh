930e7f292147d3d3a842d0edc86b8155f0656fb9
Federico Mena Quintero <federico@gnome.org>
2020-06-03 12:07:28 -0500

    Install debuggers in the openSUSE image

--- opensuse/prepare.sh
+++ opensuse/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 zypper update -y
-zypper install -y gcc rust cargo rustfmt make vala \
+zypper install -y gcc rust cargo rustfmt gdb rust-gdb rr make vala \
     automake autoconf libtool gettext itstool git \
     gtk-doc gobject-introspection-devel gtk3-devel \
     libxml2-devel libcroco-devel cairo-devel \
