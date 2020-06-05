c19a8634d51985288c77ad491693f4c62bba6f95
Jordan Petridis <jpetridis@gnome.org>
2020-06-05 13:49:15 +0300

    ci: build images with user namespaces
    The CI runners can no longer run priviledged namespaces due to
    security concenrs.
    
    Adapted the Opensuse image since its the only one we are using atm
    but the rest need to be fixed in the future as well.

--- opensuse/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-echo "Removing zypper cache"
-zypper clean -a
-
-rm -R /root/*
--- opensuse/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-zypper update -y
-zypper install -y gcc rust cargo rustfmt gdb rust-gdb rr make vala \
-    automake autoconf libtool gettext itstool git \
-    gtk-doc gobject-introspection-devel gtk3-devel \
-    libxml2-devel libcroco-devel cairo-devel \
-    pango-devel gdk-pixbuf-devel ccache gettext-tools \
-    xz
