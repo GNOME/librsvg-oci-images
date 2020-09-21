5e8a5cc884ed5c02c95861348a066251e3049535
Sven Neumann <sven@svenfoo.org>
2020-09-21 12:38:01 +0200

    Add cargo-c to all images
    It can be installed from package on opensuse and fedora.
    
    Build and install it by means of cargo on debian and ubuntu. This
    is untested as these images are currently disabled.
    
    Closes #2

--- debian/prepare.sh
+++ debian/prepare.sh
@@ -7,5 +7,7 @@ apt install -yqq --no-install-recommends \
     libgdk-pixbuf2.0-dev libgirepository1.0-dev \
     gtk-doc-tools git libgtk-3-dev ccache \
     libxml2-dev libcroco3-dev libcairo2-dev \
-    ca-certificates openssl autotools-dev \
-    libltdl-dev autopoint
+    ca-certificates openssl openssl-dev \
+    autotools-dev libltdl-dev autopoint
+
+cargo install cargo-c
